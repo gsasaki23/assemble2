572d8e4a19a70cf04dbccc9e07b4a69fc283b4bc
Gaku Sasaki <reachgaku@gmail.com>
2021-09-21 20:31:20 -0700

    firestore features

---
+++ README.md
@@ -0,0 +1,4 @@
+# Asssemble2 (Web App)
+Following https://fireship.io/lessons/firebase-quickstart/ with Version 9 Firestore Updates
+
+Last Updated: v1 by gsasaki23, Sep 2021.
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore, collection, getDocs, query, where, doc, onSnapshot, serverTimestamp, addDoc } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,18 +19,24 @@ const app = initializeApp({
     measurementId: "G-0ZSWJE6P62"
 });
 const auth = getAuth(app);
-
-// HTML Components
-const whenSignedIn = document.getElementById('whenSignedIn');
-const whenSignedOut = document.getElementById('whenSignedOut');
-const signInBtn = document.getElementById('signInBtn');
-const signOutBtn = document.getElementById('signOutBtn');
-const userDetails = document.getElementById('userDetails');
-
 const provider = new GoogleAuthProvider();
+const db = getFirestore();
+
+// Firestore
+let thingsRef;
+let unsubscribe;
 
 /// Sign in event handlers
 window.onload = () => {
+    // HTML Components
+    const whenSignedIn = document.getElementById('whenSignedIn');
+    const whenSignedOut = document.getElementById('whenSignedOut');
+    const signInBtn = document.getElementById('signInBtn');
+    const signOutBtn = document.getElementById('signOutBtn');
+    const userDetails = document.getElementById('userDetails');
+    const createThing = document.getElementById('createThing');
+    const thingsList = document.getElementById('thingsList');
+    
     // Sign In Button Listener
     signInBtn.onclick = () => signInWithPopup(auth, provider)
         .then(result => {
@@ -48,17 +55,72 @@ window.onload = () => {
     // Sign Out Button Listener
     signOutBtn.onclick = () => auth.signOut();
     
-    auth.onAuthStateChanged(user => {
+    // Listener for Sign In/Out Sections
+    auth.onAuthStateChanged(async (user) => {
+        // Signed In
         if (user) {
-            // signed in
+            // HTML Changes
             whenSignedIn.hidden = false;
             whenSignedOut.hidden = true;
             userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
-        } else {
-            // not signed in
+
+            // Database Reference
+            // thingsRef = db.collection('things');
+
+            // Create Button Listener
+            createThing.onclick = async () => {
+                try {
+                    const thingsRef = await addDoc(collection(db, "things"), {
+                        uid: user.uid,
+                        name: "TEST TEST",
+                        // name: faker.commerce.productName(),
+                        createdAt: serverTimestamp()
+                    });
+                    console.log("Document written with ID: ", thingsRef.id);
+                } catch (e) {
+                    console.error("Error adding document: ", e);
+                }
+            }
+
+            // Query to retrieve all documents in "things" collection?
+            const querySnapshot = await getDocs(collection(db, "things"));
+            querySnapshot.forEach((doc) => {
+                console.log(`${doc.id} => ${doc.data()}`);
+            });
+            
+            // Listener for all documents where uid = logged-in user
+            const q = query(collection(db, "things"), where("uid", "==", user.uid));
+            unsubscribe = onSnapshot(q, (querySnapshot) => {
+                // Update HTML
+                const items = [];
+                querySnapshot.forEach((doc) => {
+                    items.push(`<li>${doc.id + " " + doc.data().name}</li>`);
+                });
+                thingsList.innerHTML = items.join('');
+
+                // Tracking Changes between snapshots
+                querySnapshot.docChanges().forEach((change) => {
+                    if (change.type === "added") {
+                        console.log("New thing: ", change.doc.data());
+                    }
+                    if (change.type === "modified") {
+                        console.log("Modified thing: ", change.doc.data());
+                    }
+                    if (change.type === "removed") {
+                        console.log("Removed thing: ", change.doc.data());
+                    }
+                });
+            });
+        } 
+        // Signed Out
+        else {
+            // HTML Changes
             whenSignedIn.hidden = true;
             whenSignedOut.hidden = false;
             userDetails.innerHTML = '';
+
+            // Unsubscribe from Firestore Realtime Stream
+            unsubscribe();
         }
     });
 
